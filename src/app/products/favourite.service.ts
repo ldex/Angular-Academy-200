d1bcbd0c8819d3641d9fccdb6e0c609b83855d11
ldex <laurent.duveau+git@gmail.com>
2020-06-12 16:41:41 -0400

    End of training

--- src/app/products/favourite.service.ts
+++ src/app/products/favourite.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 import { Product } from './product.interface';
+import { filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,22 @@ export class FavouriteService {
 
   constructor() { }
 
+  private addedFavourite = new BehaviorSubject<Product>(null);
+  addedFavourite$: Observable<Product> = 
+                this.addedFavourite.asObservable()
+                      .pipe(
+                        filter(product => product != null)
+                      );
+
   private favourites: Set<Product> = new Set();
+
+  resetAddedFavourite() {
+    this.addedFavourite.next(null);
+  }
 
   addToFavourites(product: Product) {
     this.favourites.add(product);
+    this.addedFavourite.next(product);
   }
 
   getFavouritesNb(): number {
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
 import { Observable, EMPTY, combineLatest, Subscription } from 'rxjs';
-import { tap, catchError, startWith, count, flatMap, map, debounceTime, filter } from 'rxjs/operators';
+import { tap, catchError, startWith, count, flatMap, map, debounceTime, filter, distinctUntilChanged } from 'rxjs/operators';
 
 import { Product } from '../product.interface';
 import { ProductService } from '../product.service';
@@ -17,14 +17,29 @@ export class ProductListComponent implements OnInit {
 
   title: string = 'Products';
   selectedProduct: Product;
-  productsNb = 0;
   errorMessage;
+  filter: FormControl = new FormControl("");
+
+  message$ = this
+  .favouriteService
+  .addedFavourite$
+  .pipe(
+    tap(console.log),
+    map(product => `Product ${product.name} added to favourites!`),
+    tap(() => this.favouriteService.resetAddedFavourite())
+  );
 
   // Pagination
   pageSize = 5;
   start = 0;
   end = this.pageSize;
   currentPage = 1;
+
+  firstPage() {
+    this.start = 0;
+    this.end = this.pageSize;
+    this.currentPage = 1;
+  }
 
   previousPage() {
     this.start -= this.pageSize;
@@ -56,7 +71,7 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.router.routeReuseStrategy.shouldReuseRoute = () => false
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
 
   refresh() {
@@ -68,6 +83,41 @@ export class ProductListComponent implements OnInit {
     .productService
     .products$
     .pipe(
-      tap(products => this.productsNb = products.length)
+      catchError(error => {
+        this.errorMessage = error;
+        return EMPTY;
+      })
     );
+
+  filter$ = this.filter.valueChanges
+    .pipe(
+      debounceTime(500),
+      map(text => text.trim()),
+      filter(text => text == '' || text.length > 2),
+      distinctUntilChanged(),
+      startWith(""),
+      tap(term => this.firstPage())
+    )
+
+  filtered$ = this.filter$
+                  .pipe(
+                    map(text => text.length > 0)
+                  );
+
+  filteredProducts$ = combineLatest([this.products$, this.filter$])
+    .pipe(
+      map(([products, filterString]) =>
+        products.filter(product => 
+          product.name.toLowerCase().includes(filterString.toLowerCase())
+        )
+      )
+    )
+
+  productsNb$: Observable<number> = 
+                  this
+                    .filteredProducts$
+                    .pipe(
+                      map(products => products.length),
+                      startWith(0)
+                    );
 }
